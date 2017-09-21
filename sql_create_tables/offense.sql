7efe8af43e5602f92b824e0779657c6f28cea8a3
capitolmuckrakr <alex@capitolmuckraker.com>
2017-09-21 16:20:11 -0700

    comment out arrest table FK, offenseclasscode FK

--- sql_create_tables/offense.sql
+++ sql_create_tables/offense.sql
@@ -1,10 +1,10 @@
 CREATE TABLE arrests.offense (
-arrest_rec_num int REFERENCES arrests.arrest (arrest_rec_num),
+arrest_rec_num int, --REFERENCES arrests.arrest (arrest_rec_num),
 pcn text,
 citation_num text,
 court_docket text,
 noc text,
-offense_class text REFERENCES arrests.offenseclasscode (code),
+offense_class text, --REFERENCES arrests.offenseclasscode (code),
 reg_status text,
 amended_date date,
 amended_noc text,
