5303adb990e4cb29353b3cbe6abee817dd6e0f54
capitolmuckrakr <alex@capitolmuckraker.com>
2017-09-21 16:14:55 -0700

    include sql_dir, fix relative dir calls,cleanup name field ws, indexex

--- bin/load_tables.sh
+++ bin/load_tables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export SQL_CREATE_DIR="${HOME}/scripts/nvdpsarrests/sql_create_tables/"
 datafilebase=$HOME/data/NV_DPS_Arrests/data/edited_RECORDS_REQUEST_table
 loaders=( "arrest" "arrestcomment" "dispositioncomment" "disposition" "offense" "deathlifecode" "dispositioncode" "offenseclasscode" "base" "basesupplemental" "warrantsupplemental" "warrantoffense" "transportationcode" "warrant" )
 n=1
@@ -6,11 +7,14 @@ echo using endpoint $AWS_PG_HOST
 for loader in "${loaders[@]}"
 do
     datafile=$datafilebase$n'.txt'
-    prog=./load_$loader.sh
+    prog=/Users/acohen/scripts/nvdpsarrests/bin/load_$loader.sh
+    err=../logs/$loader'_err.log'
     n=$((n + 1))
-    > >(tee -a $loader_out.log) 2> >(tee -a $loader_err.log >&2)
     echo loading $datafile into table $loader
-    "$prog" "$datafile" &
+    "$prog" "$datafile" 2>$err &
     wait
 done
+echo cleaning up fields and indexing.
+psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -f ${SQL_CREATE_DIR}cleanup_fields.sql
+psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -f ${SQL_CREATE_DIR}indexes.sql
 #> >(tee -a out.log) 2> >(tee -a err.log >&2)
