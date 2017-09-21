1fa959d0f85a17fbf509129d39656ab78e6c7cc1
capitolmuckrakr <alex@capitolmuckraker.com>
2017-09-21 16:17:33 -0700

    comment out arrest table FK

--- sql_create_tables/dispositioncomment.sql
+++ sql_create_tables/dispositioncomment.sql
@@ -1,6 +1,6 @@
 CREATE TABLE arrests.dispositioncomment (
 disp_comment_rec_num int primary key,
-arrest_rec_num int REFERENCES arrests.arrest (arrest_rec_num),
+arrest_rec_num int,-- REFERENCES arrests.arrest (arrest_rec_num),
 offense_seq int,
 disp_comment text,
 bin int
