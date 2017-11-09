98590906bda2ba2d4bfd9e02ce373c360824af83
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-09 15:41:07 -0800

    cleanup datafile ws, change field delimiter to pipe

--- bin/edit_data_files.sh
+++ bin/edit_data_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /Users/acohen/data/NV_DPS_Arrests/data/
+cd /Users/acohen/data/NV_DPS_Arrests/data/new_data/
 for file in RECORDS_REQUEST_table*
 do
     newfile='edited_'$file
-    sed 's/\s{3,}/\n/g' $file | tail -n +2 | sed 's/""//g' | sed 's/; ;/;;/g' | sed 's/; ;/;;/g' | egrep -v '^\s' >$newfile #remove header and blank line from data file
+#    sed 's/\s{3,}/\n/g' $file | tail -n +2 | sed 's/""//g' | sed 's/; ;/;;/g' | sed 's/; ;/;;/g' | egrep -v '^\s' >$newfile #remove header and blank line from data file
+    sed 's/  .//g' $file | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' >$newfile #remove end of line ws from data file, change delimiter to pipe, clean up line endings
 done
