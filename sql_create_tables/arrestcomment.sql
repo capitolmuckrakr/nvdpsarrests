d10c981bdd5407011c615cee858644bdb522348a
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-09 16:25:39 -0800

    remove pkey from arrestcomment bc of dup val

--- sql_create_tables/arrestcomment.sql
+++ sql_create_tables/arrestcomment.sql
@@ -1,5 +1,5 @@
 CREATE TABLE arrests.arrestcomment (
-arr_comment_rec_num int primary key,
+arr_comment_rec_num int,
 arrest_rec_num int,-- REFERENCES arrests.arrest (arrest_rec_num),
 arr_comment text,
 bin int
