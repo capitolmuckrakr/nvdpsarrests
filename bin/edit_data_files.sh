e007c919a809971811c003789299fed0522f9038
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-10 09:12:49 -0800

    remove quote chars from empty fields

--- bin/edit_data_files.sh
+++ bin/edit_data_files.sh
@@ -4,5 +4,5 @@ for file in RECORDS_REQUEST_table*
 do
     newfile='edited_'$file
 #    sed 's/\s{3,}/\n/g' $file | tail -n +2 | sed 's/""//g' | sed 's/; ;/;;/g' | sed 's/; ;/;;/g' | egrep -v '^\s' >$newfile #remove header and blank line from data file
-    bomstrip $file | sed 's/  .//g' | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' >$newfile #remove bom, end of line ws from data file, change delimiter to pipe, clean up line endings
+    bomstrip $file | sed 's/  .//g' | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' | sed 's/\^\^//g' >$newfile #remove bom, end of line ws from data file, change delimiter to pipe, clean up line endings, remove empty quoted fields
 done
