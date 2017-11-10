4e60ceb2b1e92c0f1505b86c38d6f41667d8ebb5
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-09 17:37:08 -0800

    remove bom from data files with bomstrip

--- bin/edit_data_files.sh
+++ bin/edit_data_files.sh
@@ -4,5 +4,5 @@ for file in RECORDS_REQUEST_table*
 do
     newfile='edited_'$file
 #    sed 's/\s{3,}/\n/g' $file | tail -n +2 | sed 's/""//g' | sed 's/; ;/;;/g' | sed 's/; ;/;;/g' | egrep -v '^\s' >$newfile #remove header and blank line from data file
-    sed 's/  .//g' $file | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' >$newfile #remove end of line ws from data file, change delimiter to pipe, clean up line endings
+    bomstrip $file | sed 's/  .//g' | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' >$newfile #remove bom, end of line ws from data file, change delimiter to pipe, clean up line endings
 done
