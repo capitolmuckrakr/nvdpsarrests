a81ba5d851bd8ed1d634e8e27af77f6aff859dab
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-09 16:00:09 -0800

    use quoting in postgres copy statement, pipe delimiter

--- bin/load_dispositioncomment.sh
+++ bin/load_dispositioncomment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CSV_PATH=$1
-cat $CSV_PATH | psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -c "\copy arrests.dispositioncomment FROM '$CSV_PATH' WITH DELIMITER ';' CSV NULL AS ''"
+cat $CSV_PATH | psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -c "\copy arrests.dispositioncomment FROM '$CSV_PATH' WITH NULL '' DELIMITER '|' CSV QUOTE '^'  "
--- bin/load_warrant.sh
+++ bin/load_warrant.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CSV_PATH=$1
-cat $CSV_PATH | psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -c "\copy warrants.warrant FROM '$CSV_PATH' WITH DELIMITER ';' CSV NULL AS ''"
+cat $CSV_PATH | psql --host=${AWS_PG_HOST} --port=5432 --username=acohen --dbname=nvdpsarrests -c "\copy warrants.warrant FROM '$CSV_PATH' WITH NULL '' DELIMITER '|' CSV QUOTE '^'  "
