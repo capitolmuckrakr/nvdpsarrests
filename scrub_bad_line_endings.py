6d451fb701a85bdf0fab516b7ccf50fe5f607d8d
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-10 16:25:32 -0800

    python script to remove cr/lf inside fields

---
+++ scrub_bad_line_endings.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import re, sys, os
+pattern = re.compile(r'([A-Za-z0-9])\r\n')
+file = sys.argv[1]
+outfile = 'processed_' + file
+with open(file) as infile:
+    with open(outfile,'w') as out:
+        for line in infile.readlines():
+            line2 = pattern.sub(r'\1',line)
+            out.write(line2)
+            
