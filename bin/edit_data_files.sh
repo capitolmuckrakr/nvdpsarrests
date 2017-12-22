70b6a594e239927f9b4fab47ea025af15e5a484c
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-21 16:13:58 -0800

    processing of data files needed to load them into sql

--- bin/edit_data_files.sh
+++ bin/edit_data_files.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /Users/acohen/data/NV_DPS_Arrests/data/new_data/
-for file in RECORDS_REQUEST_table*
+for file in table*
 do
     newfile='edited_'$file
     processed='scrubbed_'$file
     python /Users/acohen/scripts/nvdpsarrests/scrub_bad_line_endings.py $file #remove cr/lf from fields
-#    sed 's/\s{3,}/\n/g' $file | tail -n +2 | sed 's/""//g' | sed 's/; ;/;;/g' | sed 's/; ;/;;/g' | egrep -v '^\s' >$newfile #remove header and blank line from data file
-    bomstrip $processed | sed 's/  .//g' | egrep -v '^\s' | sed 's/\^ \^/\^\|\^/g' | sed 's/\r/''/g' | sed 's/\^\^//g' >$newfile #remove bom, end of line ws from data file, change delimiter to pipe, clean up line endings, remove empty quoted fields
+    sed 's/^~//g' $processed >$newfile
 done
