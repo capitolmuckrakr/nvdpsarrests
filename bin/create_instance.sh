c88e708e0aca0483d5b631b6b0f51338de95b985
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-21 16:12:41 -0800

    export all AWS env vars

--- bin/create_instance.sh
+++ bin/create_instance.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 # You need to install the AWS Command Line Interface from http://aws.amazon.com/cli/
-VPCID=$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)
+export VPCID=$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)
 echo $VPCID
-SUBNETID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)
+export SUBNETID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)
 echo $SUBNETID
-SGID=$(aws ec2 create-security-group --group-name nvdpssecuritygroup --description "NV DPS arrests Postgres security group" --vpc-id $VPCID --output text)
+export SGID=$(aws ec2 create-security-group --group-name nvdpssecuritygroup --description "NV DPS arrests Postgres security group" --vpc-id $VPCID --output text)
 echo $SGID
 aws ec2 authorize-security-group-ingress --group-id $SGID --protocol tcp --port 5432 --cidr 0.0.0.0/0
 
-INSTANCEID=$(aws rds create-db-instance \
+export INSTANCEID=$(aws rds create-db-instance \
     --db-name nvdpsarrests \
     --vpc-security-group-ids $SGID \
     --db-instance-identifier nvarrests \
@@ -23,11 +23,9 @@ echo "waiting for $INSTANCEID ..."
 
 aws rds wait db-instance-available --db-instance-identifier $INSTANCEID
 
-ENDPOINT=$(aws rds describe-db-instances --db-instance-identifier $INSTANCEID --query "DBInstances[0].Endpoint.Address" --output text)
+export ENDPOINT=$(aws rds describe-db-instances --db-instance-identifier $INSTANCEID --query "DBInstances[0].Endpoint.Address" --output text)
 
 echo "$INSTANCEID is available and listening on $ENDPOINT"
-
-export INSTANCEID="${INSTANCEID}"
 
 export AWS_PG_HOST="${ENDPOINT}"
 
