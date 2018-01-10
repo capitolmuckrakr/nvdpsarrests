7b74eae994e8a0e6eb517c59c087414cadd0e498
capitolmuckrakr <alex@capitolmuckraker.com>
2018-01-09 18:09:43 -0800

    add permissions for team to view database

---
+++ sql_create_tables/iteam.sql
@@ -0,0 +1,12 @@
+CREATE ROLE readaccess;
+GRANT USAGE ON SCHEMA public to readaccess;
+GRANT USAGE ON SCHEMA arrests to readaccess;
+GRANT USAGE ON SCHEMA warrants to readaccess;
+GRANT SELECT ON ALL TABLES IN SCHEMA public to readaccess;
+GRANT SELECT ON ALL TABLES IN SCHEMA arrests to readaccess;
+GRANT SELECT ON ALL TABLES IN SCHEMA warrants to readaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA arrests GRANT SELECT ON TABLES TO readaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA warrants GRANT SELECT ON TABLES TO readaccess;
+GRANT readaccess TO iteam;
+
