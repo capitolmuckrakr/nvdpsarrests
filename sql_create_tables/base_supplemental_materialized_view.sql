97fe8779b42a085cdaef18063e9ce4b7b46214ea
capitolmuckrakr <alex@capitolmuckraker.com>
2018-01-16 15:49:03 -0800

    fix table name in base supplemental materialized view

--- sql_create_tables/base_supplemental_materialized_view.sql
+++ sql_create_tables/base_supplemental_materialized_view.sql
@@ -1,33 +1,33 @@
-CREATE MATERIALIZED VIEW public.warrants_basesupplemental
+CREATE MATERIALIZED VIEW public.warrants_base_supplemental
 AS
 SELECT pk,nin,aka,cau,ccf,adb,asn,smt,smc
 FROM warrants.basesupplemental
 WITH DATA;
-ALTER TABLE public.warrants_basesupplemental
+ALTER TABLE public.warrants_base_supplemental
 OWNER TO acohen;
-GRANT ALL ON TABLE public.warrants_basesupplemental TO acohen;
-GRANT SELECT ON TABLE public.warrants_basesupplemental TO readaccess;
+GRANT ALL ON TABLE public.warrants_base_supplemental TO acohen;
+GRANT SELECT ON TABLE public.warrants_base_supplemental TO readaccess;
 
-CREATE INDEX pub_warrants_basesupplemental_pk_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_pk_idx
+ON public.warrants_base_supplemental
 USING btree (pk);
 
-CREATE INDEX pub_warrants_basesupplemental_nin_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_nin_idx
+ON public.warrants_base_supplemental
 USING btree (nin);
 
-CREATE INDEX pub_warrants_basesupplemental_aka_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_aka_idx
+ON public.warrants_base_supplemental
 USING btree (aka);
 
-CREATE INDEX pub_warrants_basesupplemental_ccf_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_ccf_idx
+ON public.warrants_base_supplemental
 USING btree (ccf);
 
-CREATE INDEX pub_warrants_basesupplemental_adb_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_adb_idx
+ON public.warrants_base_supplemental
 USING btree (adb);
 
-CREATE INDEX pub_warrants_basesupplemental_asn_idx
-ON public.warrants_basesupplemental
+CREATE INDEX pub_warrants_base_supplemental_asn_idx
+ON public.warrants_base_supplemental
 USING btree (asn);
