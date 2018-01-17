39f58a851d16b6596b9230a017080546a12c00bd
capitolmuckrakr <alex@capitolmuckraker.com>
2018-01-17 09:19:18 -0800

    fix warrants schema name

--- sql_create_tables/warrant_offense_materialized_view.sql
+++ sql_create_tables/warrant_offense_materialized_view.sql
@@ -1,49 +1,49 @@
-CREATE MATERIALIZED VIEW public.warrant_offenses
+CREATE MATERIALIZED VIEW public.warrants_offenses
 AS
 SELECT pk,offense_rec_num,warrant_rec_num,offense_seq,noc_5,original_noc_5,cash_bail,surety_bail,offense_comment,entering_agency_ori,local_charge_num
 FROM warrants.warrantoffense
 WITH DATA;
-ALTER TABLE public.warrant_offenses
+ALTER TABLE public.warrants_offenses
 OWNER TO acohen;
-GRANT ALL ON TABLE public.warrant_offenses TO acohen;
-GRANT SELECT ON TABLE public.warrant_offenses TO readaccess;
+GRANT ALL ON TABLE public.warrants_offenses TO acohen;
+GRANT SELECT ON TABLE public.warrants_offenses TO readaccess;
 
 CREATE INDEX pub_warrant_offenses_pk_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (pk);
 
 CREATE INDEX pub_warrant_offenses_offense_rec_num_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (offense_rec_num);
 
 CREATE INDEX pub_warrant_offenses_warrant_rec_num_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (warrant_rec_num);
 
 CREATE INDEX pub_warrant_offenses_offense_seq_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (offense_seq);
 
 CREATE INDEX pub_warrant_offenses_noc_5_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (noc_5);
 
 CREATE INDEX pub_warrant_offenses_original_noc_5_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (original_noc_5);
 
 CREATE INDEX pub_warrant_offenses_cash_bail_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (cash_bail);
 
 CREATE INDEX pub_warrant_offenses_surety_bail_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (surety_bail);
 
 CREATE INDEX pub_warrant_offenses_entering_agency_ori_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (entering_agency_ori);
 
 CREATE INDEX pub_warrant_offenses_local_charge_num_idx
-ON public.warrant_offenses
+ON public.warrants_offenses
 USING btree (local_charge_num);
--- sql_create_tables/warrant_supplemental_materialized_view.sql
+++ sql_create_tables/warrant_supplemental_materialized_view.sql
@@ -1,61 +1,61 @@
-CREATE MATERIALIZED VIEW public.warrant_supplemental
+CREATE MATERIALIZED VIEW public.warrants_supplemental
 AS
 SELECT pk,nin,hoi,coi,wnm,noc,ofc,ftf,trf,dso,dow,oca,ccn,bai,tra,mis
 FROM warrants.warrantsupplemental
 WITH DATA;
-ALTER TABLE public.warrant_supplemental
+ALTER TABLE public.warrants_supplemental
 OWNER TO acohen;
-GRANT ALL ON TABLE public.warrant_supplemental TO acohen;
-GRANT SELECT ON TABLE public.warrant_supplemental TO readaccess;
+GRANT ALL ON TABLE public.warrants_supplemental TO acohen;
+GRANT SELECT ON TABLE public.warrants_supplemental TO readaccess;
 
 CREATE INDEX pub_warrant_supplemental_pk_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (pk);
 
 CREATE INDEX pub_warrant_supplemental_nin_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (nin);
 
 CREATE INDEX pub_warrant_supplemental_hoi_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (hoi);
 
 CREATE INDEX pub_warrant_supplemental_coi_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (coi);
 
 CREATE INDEX pub_warrant_supplemental_wnm_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (wnm);
 
 CREATE INDEX pub_warrant_supplemental_lwnm_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (LEFT(wnm,position(',' in wnm)-1));
 
 CREATE INDEX pub_warrant_supplemental_noc_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (noc);
 
 CREATE INDEX pub_warrant_supplemental_dow_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (dow);
 
 CREATE INDEX pub_warrant_supplemental_oca_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (oca);
 
 CREATE INDEX pub_warrant_supplemental_ccn_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (ccn);
 
 CREATE INDEX pub_warrant_supplemental_bai_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (bai);
 
 CREATE INDEX pub_warrant_supplemental_tra_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (tra);
 
 CREATE INDEX pub_warrant_supplemental_mis_idx
-ON public.warrant_supplemental
+ON public.warrants_supplemental
 USING btree (mis);
